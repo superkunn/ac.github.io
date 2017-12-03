e3112f763f31409d86e8a38851fa9416759cc02e
superkunn <1150527505@qq.com>
2017-12-03 22:36:03 +0800

    push

--- upoj_n.py
+++ upoj_n.py
@@ -51,14 +51,33 @@ print(poj_n)
 
 # hdu
 
+my_id = "hanxukun"
+url = "http://acm.hdu.edu.cn/userstatus.php?user=" + my_id
+wbdata = requests.get(url).text
+soup = BeautifulSoup(wbdata,'lxml')
+x = soup.select("td")
+pattern = '<td align="center">\d.*?</td>'
+i=1
+for line in x:
+    sline = str(line)
+    ret = re.search(pattern, sline)
+    if ret:
+        print(sline)
+        if(i==5):
+            hdu_n = int(re.sub("\D", "", sline))
+            break
+        i+=1
+sum_n = sum_n + hdu_n
+print(hdu_n)
+
 
 
 
 
 # write
 b_text = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>渣渣的题量</title><script type="text/javascript" src="jQuery.js"></script><script type="text/javascript" src="jqplot.js"></script></head><body><div style="text-align:center;clear:both;"><script src="/gg_bd_ad_720x90.js" type="text/javascript"></script><script src="/follow.js" type="text/javascript"></script></div><div id="chart2"></div><script type="text/javascript">var data = [['
-m_text = str(sum_n) + ',' + str(nyist_n) + ',' + str(sdut_n) + ',' + str(poj_n)
-e_text = ']];var data_max = 500; var line_title = ["AC题数"]; var y_label = "AC题数"; var x_label = "oj"; var x = ["总量","nyist","sdut","poj"]; var title = "渣渣的题量";j.jqplot.diagram.base("chart2", data, line_title, "渣渣的题量，自动统计", x, x_label, y_label, data_max, 2);</script></body></html>'
+m_text = str(sum_n) + ',' + str(nyist_n) + ',' + str(sdut_n) + ',' + str(poj_n) + ',' str(hdu_n)
+e_text = ']];var data_max = 500; var line_title = ["AC题数"]; var y_label = "AC题数"; var x_label = "oj"; var x = ["总量","nyist","sdut","poj","hdu"]; var title = "渣渣的题量";j.jqplot.diagram.base("chart2", data, line_title, "渣渣的题量，自动统计", x, x_label, y_label, data_max, 2);</script></body></html>'
 all_the_text = b_text + m_text + e_text
 file_object = open('index.html', 'w')
 file_object.write(all_the_text)
